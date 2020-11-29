c87e9aebae16050da8ccaf927b9d7b245ef21564
Ryan Zheng <nogrethumphrey@gmail.com>
2020-11-29 17:06:07 +0100

    use comma as separate

--- news_search/pods/text_loader.py
+++ news_search/pods/text_loader.py
@@ -6,7 +6,8 @@ from typing import Dict
 from jina.executors.crafters import BaseCrafter
 
 
+##The first row has to be dropped
 class TextExtractor(BaseCrafter):
     def craft(self, text: str, *args, **kwargs) -> Dict:
-        character, sentence = text.split('[SEP]')
+        character, sentence = text.split(',', 1)
         return dict(weight=1., text=sentence, meta_info=character.encode('utf8'))
